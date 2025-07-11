72ce6acb1099c53f6381f6cc8b9d22d08e7c0a08
AutumnMood924 <gloopthemaster@gmail.com>
2025-07-11 19:50:29 -0400

    config (real)

--- amm.lua
+++ amm.lua
@@ -19,6 +19,22 @@ AMM.config = {
 	},
 }
 
+-- actual config
+AMM.mod.config_tab = function()
+    return {n = G.UIT.ROOT, config = {align = "m", r = 0.1, padding = 0.1, colour = G.C.BLACK, minw = 8, minh = 6}, nodes = {
+        {n = G.UIT.R, config = {align = "cl", padding = 0, minh = 0.1}, nodes = {}},
+
+        {n = G.UIT.R, config = {align = "cl", padding = 0}, nodes = {
+            {n = G.UIT.C, config = { align = "cl", padding = 0.05 }, nodes = {
+                create_toggle{ col = true, label = "", scale = 1, w = 0, shadow = true, ref_table = AMM.mod.config, ref_value = "joyousspringify" },
+            }},
+            {n = G.UIT.C, config = { align = "c", padding = 0 }, nodes = {
+                { n = G.UIT.T, config = { text = "JoyousSpringify many Jokers", scale = 0.45, colour = G.C.UI.TEXT_LIGHT }},
+            }},
+        }},
+    }}
+end
+
 SMODS.Atlas {
 	key = "modicon",
 	path = "OddityTag.png",
